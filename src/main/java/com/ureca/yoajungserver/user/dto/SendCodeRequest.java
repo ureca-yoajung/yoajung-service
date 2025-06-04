06dfb3e08e795a1562315f02f9889a367b818c89
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-06-05 02:27:11 +0900

    feat: add SendCodeRequest

---
+++ src/main/java/com/ureca/yoajungserver/user/dto/SendCodeRequest.java
@@ -0,0 +1,18 @@
+package com.ureca.yoajungserver.user.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SendCodeRequest {
+    @Email(message = "이메일 형식을 지키세요")
+    @NotBlank(message = "필수 입력입니다")
+    String email;
+}
+
+// 이메일 인증 코드 요청 시 바디로 사용
