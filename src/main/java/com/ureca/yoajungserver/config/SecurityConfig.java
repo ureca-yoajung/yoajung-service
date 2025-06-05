4e3acfb3c2089901e9212ccc77fb8ff8c2bad6bf
Eddie Lee <devleehy@gmail.com>
2025-06-05 17:53:52 +0900

    fix: SecurityConfig 수정

--- src/main/java/com/ureca/yoajungserver/config/SecurityConfig.java
+++ src/main/java/com/ureca/yoajungserver/config/SecurityConfig.java
@@ -16,6 +16,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
+                                "/**",
                                 "/api/auth/**",
                                 "/api/user/signup"
                         ).permitAll()
