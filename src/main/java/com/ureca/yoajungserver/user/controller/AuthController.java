2bd9a0423cb861a1debf40ebfc508ac08afee738
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-06-05 10:45:38 +0900

    feat: add AuthController 인증코드 요청 분기

---
+++ src/main/java/com/ureca/yoajungserver/user/controller/AuthController.java
@@ -0,0 +1,33 @@
+package com.ureca.yoajungserver.user.controller;
+
+import com.ureca.yoajungserver.common.ApiResponse;
+import com.ureca.yoajungserver.common.BaseCode;
+import com.ureca.yoajungserver.user.dto.reqeust.SendCodeRequest;
+import com.ureca.yoajungserver.user.dto.reqeust.VerifyCodeRequest;
+import com.ureca.yoajungserver.user.service.AuthService;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/api/auth/send-code")
+    public ResponseEntity<ApiResponse<Void>> sendCode(@Valid @RequestBody SendCodeRequest request, HttpSession session) {
+        authService.sendVerificationCode(request, session);
+        return ResponseEntity.ok(ApiResponse.ok(BaseCode.EMAIL_CODE_SENT));
+    }
+    //  사용자가 이메일 입력하고 인증 코드 받기 요청
+
+    @PostMapping("/api/auth/verify-code")
+    public ResponseEntity<ApiResponse<Void>> verifyCode(@Valid @RequestBody VerifyCodeRequest request, HttpSession httpSession) {
+        authService.verifyCode(request, httpSession);
+        return ResponseEntity.ok(ApiResponse.ok(BaseCode.EMAIL_VERIFICATION_SUCCESS));
+    }
+}
