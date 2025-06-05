d8b419eba1688c9f8a114f88cbc9f2301886e895
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-06-05 10:45:52 +0900

    feat: add UserController 회원가입 요청 분기

---
+++ src/main/java/com/ureca/yoajungserver/user/controller/UserController.java
@@ -0,0 +1,28 @@
+
+package com.ureca.yoajungserver.user.controller;
+
+import com.ureca.yoajungserver.common.ApiResponse;
+import com.ureca.yoajungserver.common.BaseCode;
+import com.ureca.yoajungserver.user.dto.reqeust.SignupRequest;
+import com.ureca.yoajungserver.user.service.UserService;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/api/user/signup")
+    public ResponseEntity<ApiResponse<Void>> signup(@Valid @RequestBody SignupRequest request, HttpSession session) {
+        userService.signup(request, session);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.ok(BaseCode.STATUS_SIGNUP_SUCCESS));
+    }
+}
