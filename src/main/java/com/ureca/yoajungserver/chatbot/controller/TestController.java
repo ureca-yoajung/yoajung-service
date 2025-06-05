f5ba00fa9a1aaa5da2bd551a4e4d9f52f6a4a4d0
iju42829 <ijud4282@gmail.com>
2025-06-05 11:31:26 +0900

    feat: MySQL 기반 멀티턴 대화 저장 구현

--- src/main/java/com/ureca/yoajungserver/chatbot/controller/TestController.java
+++ src/main/java/com/ureca/yoajungserver/chatbot/controller/TestController.java
@@ -1,15 +1,23 @@
 package com.ureca.yoajungserver.chatbot.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class TestController {
 
-    @GetMapping("/test")
-    public String testBadWord(@RequestParam("question") String question) {
+    private final ChatClient chatClient;
 
-        return question;
+    @GetMapping("/test")
+    public String testBadWord(@RequestParam("question") String question, @RequestParam("userId") Long userId) {
+        return chatClient.prompt().user(question)
+                .advisors(advisorSpec -> advisorSpec.param(ChatMemory.CONVERSATION_ID, userId))
+                .call()
+                .content();
     }
 }
