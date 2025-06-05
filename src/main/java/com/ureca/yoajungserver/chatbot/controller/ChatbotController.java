6abdf8a0234824b5f7b5ebb5106eea0d8342e26d
Eddie Lee <devleehy@gmail.com>
2025-06-05 15:28:50 +0900

    feat: ChatbotController 구현

---
+++ src/main/java/com/ureca/yoajungserver/chatbot/controller/ChatbotController.java
@@ -0,0 +1,25 @@
+package com.ureca.yoajungserver.chatbot.controller;
+
+import static com.ureca.yoajungserver.common.BaseCode.KEYWORD_MAPPING_SUCCESS;
+
+import com.ureca.yoajungserver.chatbot.dto.PlanKeywordResponse;
+import com.ureca.yoajungserver.chatbot.service.ChatbotService;
+import com.ureca.yoajungserver.common.ApiResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ChatbotController {
+
+    private final ChatbotService chatbotService;
+
+    @GetMapping("/chat")
+    public ResponseEntity<ApiResponse<PlanKeywordResponse>> getPlanRecommendation(@RequestParam("input") String input) throws IOException {
+        return ResponseEntity.ok(ApiResponse.of(KEYWORD_MAPPING_SUCCESS, chatbotService.keywordMapper(input)));
+    }
+}
