1bb97958315979c414f488da028af0426be09851
iju42829 <ijud4282@gmail.com>
2025-06-09 10:34:52 +0900

    feat: 욕설 필터링 분리

--- src/main/java/com/ureca/yoajungserver/common/config/WebConfig.java
+++ src/main/java/com/ureca/yoajungserver/common/config/WebConfig.java
@@ -2,8 +2,9 @@ package com.ureca.yoajungserver.common.config;
 
 import com.ureca.yoajungserver.chatbot.interceptor.GptBadWordInterceptor;
 import com.ureca.yoajungserver.chatbot.interceptor.LocalBadWordInterceptor;
+import com.ureca.yoajungserver.common.component.GptBadWordComponent;
+import com.ureca.yoajungserver.common.component.LocalBadWordComponent;
 import lombok.RequiredArgsConstructor;
-import org.springframework.ai.openai.OpenAiModerationModel;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,16 +13,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
-    private final OpenAiModerationModel moderationModel;
+    private final GptBadWordComponent gptBadWordComponent;
+    private final LocalBadWordComponent localBadWordComponent;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new GptBadWordInterceptor(moderationModel))
+        registry.addInterceptor(new GptBadWordInterceptor(gptBadWordComponent))
                 .order(1)
                 .addPathPatterns("/test")
                 .excludePathPatterns("/css/**", "/*.ico", "/error");
 
-        registry.addInterceptor(new LocalBadWordInterceptor())
+        registry.addInterceptor(new LocalBadWordInterceptor(localBadWordComponent))
                 .order(2)
                 .addPathPatterns("/test")
                 .excludePathPatterns("/css/**", "/*.ico", "/error");
