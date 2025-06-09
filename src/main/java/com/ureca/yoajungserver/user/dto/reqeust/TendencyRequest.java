3c4c86982bf4f19664237e79f54c4525536901ee
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-06-09 22:42:47 +0900

    feat: add TendencyDto

---
+++ src/main/java/com/ureca/yoajungserver/user/dto/reqeust/TendencyRequest.java
@@ -0,0 +1,19 @@
+package com.ureca.yoajungserver.user.dto.reqeust;
+
+import jakarta.validation.constraints.Min;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class TendencyRequest {
+    @Min(value = 0, message = "0 이상")
+    private Integer avgMonthLyDataGB;
+    @Min(value = 0, message = "0 이상")
+    private Integer avgMonthlyVoiceMin;
+
+    private String comment;
+
+    @Min(value = 0, message = "0 이상")
+    private Integer preferencePrice;
+}
---
+++ src/main/java/com/ureca/yoajungserver/user/dto/response/TendencyResponse.java
@@ -0,0 +1,25 @@
+package com.ureca.yoajungserver.user.dto.response;
+
+import com.ureca.yoajungserver.user.entity.Tendency;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class TendencyResponse {
+    private final Integer avgMonthlyDataGB;
+    private final Integer avgMonthlyVoiceMin;
+    private final String comment;
+    private final Integer preferencePrice;
+
+    public static TendencyResponse fromEntity(Tendency tendency) {
+        return TendencyResponse.builder()
+                .avgMonthlyDataGB(tendency.getAvgMonthlyDataGB())
+                .avgMonthlyDataGB(tendency.getAvgMonthlyVoiceMin())
+                .comment(tendency.getComment())
+                .preferencePrice(tendency.getPreferencePrice())
+                .build();
+    }
+}
