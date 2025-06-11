e8e6a5377b1615883470c208bfeddb6d4eabe18d
seokjuun <seokjuun@gmail.com>
2025-06-11 15:37:00 +0900

    feat: 요금제 통계 entity, repository 추가

---
+++ src/main/java/com/ureca/yoajungserver/plan/repository/PlanStatisticRepository.java
@@ -0,0 +1,7 @@
+package com.ureca.yoajungserver.plan.repository;
+
+import com.ureca.yoajungserver.plan.entity.PlanStatistic;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlanStatisticRepository extends JpaRepository<PlanStatistic, Long> {
+}
