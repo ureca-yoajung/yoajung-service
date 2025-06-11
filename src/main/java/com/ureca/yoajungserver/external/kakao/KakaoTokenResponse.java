7cbe7921fa7ca2cd963834a35dda59d40ee0ea81
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-06-12 02:03:21 +0900

    feat: add KakaoDTO token, signup

---
+++ src/main/java/com/ureca/yoajungserver/external/kakao/KakaoTokenResponse.java
@@ -0,0 +1,10 @@
+package com.ureca.yoajungserver.external.kakao;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class KakaoTokenResponse {
+    private String access_token;
+}
