dfcbc92f253f896b856599e2dc3a3f9aa7d9a73e
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-06-12 16:55:39 +0900

    feat: add jacksonConfig for serialization

--- src/main/java/com/ureca/yoajungserver/config/SecurityConfig.java
+++ src/main/java/com/ureca/yoajungserver/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.ureca.yoajungserver.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ureca.yoajungserver.common.ApiResponse;
+import com.ureca.yoajungserver.common.BaseCode;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,13 +14,47 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+    private final ObjectMapper objectMapper;
+
+    public SecurityConfig(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session
+                        .invalidSessionUrl("/login.html")
+                        .maximumSessions(1)
+                )
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
+                        .requestMatchers(
+                                "/login.html", "/signup.html", "/reset-password.html"
+                                , "/reset-request.html", "main.html",
+                                "/api/oauth/**", "/api/auth/**", "callback.html"
+                        ).permitAll()
+                        .requestMatchers(
+                                "/api/user/**",
+                                "/api/tendency/**"
+                        ).authenticated()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint((request, response, authException) -> {
+                            response.setStatus(BaseCode.STATUS_UNAUTHORIZED.getStatus().value());
+                            response.setContentType("application/json; charset=UTF-8");
+
+                            ApiResponse<Void> apiResponse = ApiResponse.ok(BaseCode.STATUS_UNAUTHORIZED);
+                            objectMapper.writeValue(response.getWriter(), apiResponse);
+                        })
+                        .accessDeniedHandler((request, response, accessDeniedException) -> {
+                            response.setStatus(BaseCode.STATUS_FORBIDDEN.getStatus().value());
+                            response.setContentType("application/json; charset=UTF-8");
+
+                            ApiResponse<Void> apiResponse = ApiResponse.ok(BaseCode.STATUS_FORBIDDEN);
+                            objectMapper.writeValue(response.getWriter(), apiResponse);
+                        })
                 )
         // 세션관리
         // 예외처리
