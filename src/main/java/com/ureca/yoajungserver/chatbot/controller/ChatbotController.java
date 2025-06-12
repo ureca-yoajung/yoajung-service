9344bf2b7c8df03b3af805aba5cf425d2684c3cd
Eddie Lee <devleehy@gmail.com>
2025-06-12 13:59:37 +0900

    feat: 파라미터 수정
    - 회원, 비회원을 구분하기 위해 랜덤한 guestId와 로그인된 user를 받아서 chatbotService 호출

--- src/main/java/com/ureca/yoajungserver/chatbot/controller/ChatbotController.java
+++ src/main/java/com/ureca/yoajungserver/chatbot/controller/ChatbotController.java
@@ -2,12 +2,13 @@ package com.ureca.yoajungserver.chatbot.controller;
 
 import static com.ureca.yoajungserver.common.BaseCode.KEYWORD_MAPPING_SUCCESS;
 
-import com.ureca.yoajungserver.chatbot.dto.ChatbotResponse;
 import com.ureca.yoajungserver.chatbot.service.ChatbotService;
 import com.ureca.yoajungserver.common.ApiResponse;
+import com.ureca.yoajungserver.user.security.CustomUserDetails;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,9 +20,13 @@ public class ChatbotController {
     private final ChatbotService chatbotService;
 
     @GetMapping("/chat")
-    public ResponseEntity<ApiResponse<ChatbotResponse>> getPlanRecommendation(
-            @RequestParam("input") String input,
-            @RequestParam("userId") String userId) throws IOException {
-        return ResponseEntity.ok(ApiResponse.of(KEYWORD_MAPPING_SUCCESS, chatbotService.keywordMapper(input, userId)));
+    public ResponseEntity<ApiResponse<?>> getPlanRecommendation(
+            @RequestParam("question") String question,
+            @RequestParam("guestId") String guestId,
+            @AuthenticationPrincipal CustomUserDetails user) throws IOException {
+        String id = (user != null && user.getId() != null)
+                ? user.getId().toString()
+                : guestId;
+        return ResponseEntity.ok(ApiResponse.of(KEYWORD_MAPPING_SUCCESS, chatbotService.keywordMapper(question, id)));
     }
 }
