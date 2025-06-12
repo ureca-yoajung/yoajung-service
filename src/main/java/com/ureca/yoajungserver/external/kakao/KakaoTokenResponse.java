7e41bb5e4f74beb6ecb99c86e69d60de0681a296
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-06-12 14:06:42 +0900

    fix: response dto

--- src/main/java/com/ureca/yoajungserver/external/kakao/KakaoTokenResponse.java
+++ src/main/java/com/ureca/yoajungserver/external/kakao/KakaoTokenResponse.java
@@ -7,4 +7,8 @@ import lombok.Setter;
 @Setter
 public class KakaoTokenResponse {
     private String access_token;
+    private String token_type;
+    private String refresh_token;
+    private int expires_in;
+    private String scope;
 }
