393c78ee825a00cc27a8e34a3b41b8e5ff8c1f8e
hyew0nn <hyewon-0607@naver.com>
2025-06-17 09:46:05 +0900

    fix: 상세 페이지 ui 수정, 에러 코드 리팩토링

--- src/main/java/com/ureca/yoajungserver/user/entity/User.java
+++ src/main/java/com/ureca/yoajungserver/user/entity/User.java
@@ -20,7 +20,7 @@ public class User extends BaseTimeEntity {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "planId", nullable = false)
+    @JoinColumn(name = "plan", nullable = false)
     private Plan plan;
 
     @Column(name = "name", nullable = false)
