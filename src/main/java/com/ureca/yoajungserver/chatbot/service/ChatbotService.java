25a6c0689de4ac8305e12e2f3f7b6ced55190542
so-yeon1 <psy92411@gmail.com>
2025-06-17 11:41:04 +0900

    feat: plan 조회내역 dchatmemory에 저장

--- src/main/java/com/ureca/yoajungserver/chatbot/service/ChatbotService.java
+++ src/main/java/com/ureca/yoajungserver/chatbot/service/ChatbotService.java
@@ -9,5 +9,4 @@ import java.util.List;
 public interface ChatbotService {
 
     List<PersonalPlanRecommendResponse> keywordMapper(String input, String userId) throws IOException;
-    List<PersonalPlanRecommendResponse> planList(PlanKeywordResponse keywordResponse);
 }
--- src/main/java/com/ureca/yoajungserver/chatbot/service/ChatbotServiceImpl.java
+++ src/main/java/com/ureca/yoajungserver/chatbot/service/ChatbotServiceImpl.java
@@ -1,6 +1,8 @@
 package com.ureca.yoajungserver.chatbot.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.theokanning.openai.completion.chat.ChatMessage;
+import com.theokanning.openai.completion.chat.ChatMessageRole;
 import com.ureca.yoajungserver.chatbot.dto.PersonalPlanRecommendResponse;
 import com.ureca.yoajungserver.chatbot.dto.PlanKeywordFirst;
 import com.ureca.yoajungserver.chatbot.dto.PlanKeywordResponse;
@@ -11,10 +13,20 @@ import jakarta.annotation.PostConstruct;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Map;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.MessageType;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
@@ -27,6 +39,9 @@ public class ChatbotServiceImpl implements ChatbotService {
     private final ChatClient chatClient;
     private final ObjectMapper objectMapper;
     private final ChatbotRepository chatbotRepository;
+
+    private final ChatMemory chatMemory;
+    private final JdbcChatMemoryRepository chatMemoryRepository;
 
     // application.yml 의 키와 일치시킵니다.
     @Value("${spring.ai.chat.system-prompt1}")
@@ -76,14 +91,15 @@ public class ChatbotServiceImpl implements ChatbotService {
         log.info("planKeyword3 : {}", resp3);
         log.info("planKeywordResponse : {}", planKeywordResponse);
 
-        // 예시로 첫 번째 응답을 DB 조회에 사용
-        return chatbotRepository.recommendPlans(planKeywordResponse);
-    }
+        // DB 조회
+        List<PersonalPlanRecommendResponse> planResult = chatbotRepository.recommendPlans(planKeywordResponse); // json으로
 
-    // 요금제 조회
-    @Override
-    public List<PersonalPlanRecommendResponse> planList(PlanKeywordResponse keywordResponse) {
-        return chatbotRepository.recommendPlans(keywordResponse);
+        // 조회 결과 db에 저장
+        String json = objectMapper.writeValueAsString(planResult);
+        Message message = new SystemMessage(json);
+        chatMemory.add(userId, message);
+
+        return planResult;
     }
 
     private <T> T getLLMResponse(String input, String userId, String prompt, Class<T> responseType) {
